438cbc98c7c50b12a127545542a329d27f2655d7
snowsola <127176489+snowsola@users.noreply.github.com>
2023-03-07 16:42:21 +0800

    Update types.go

--- chat/types.go
+++ chat/types.go
@@ -1,6 +1,7 @@
 package chat
 
 type Config struct {
+	BaseUrl         string `yaml:"baseUrl" json:"baseUrl" bson:"baseUrl" validate:"required"`
 	AppKey          string `yaml:"appKey" json:"appKey" bson:"appKey" validate:"required"`
 	Port            int    `yaml:"port" json:"port" bson:"port" validate:"required"`
 	IntervalSeconds int    `yaml:"intervalSeconds" json:"intervalSeconds" bson:"intervalSeconds" validate:"required"`
